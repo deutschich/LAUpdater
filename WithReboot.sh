ceea080889422ef884c6e23972bb9ede15b6338f
deutschich <140229250+deutschich@users.noreply.github.com>
2023-09-25 16:25:27 +0200

    Create WithReboot.sh

---
+++ WithReboot.sh
@@ -0,0 +1,7 @@
+echo "Willkommen bei LAUpdater (Linux Apps Updater) !"
+echo "Dieses Programm Updatet die Apps auf dem Computer automatisch."
+echo Bitte geben Sie hier das Root Passwort ein:
+sudo apt update
+sudo apt upgrade
+sudo apt autoremove
+sudo reboot
